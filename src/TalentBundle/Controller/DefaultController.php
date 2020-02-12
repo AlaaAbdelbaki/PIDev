8f83d89d32ddc668e79bd62a875417f5937cfe2f
AlaaAbdelbaki <44524232+AlaaAbdelbaki@users.noreply.github.com>
2020-02-12 17:14:52 +0100

    Created Template

--- src/TalentBundle/Controller/DefaultController.php
+++ src/TalentBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('TalentBundle:Default:index.html.twig');
+        return $this->render('@Talent/Default/profile.html.twig');
     }
 }
