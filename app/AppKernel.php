db166bc5cfbcd9884e6624a9a8e25032e0fc779f
Anis Bouaziz <bouaziz609@gmail.com>
2020-02-13 11:33:35 +0100

    added attr to entity

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-            new CompetitionBundle\CompetitionBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
