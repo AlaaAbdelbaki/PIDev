38a2555518cf15103c50d5e499563efbbf63d3d8
AlaaAbdelbaki <44524232+AlaaAbdelbaki@users.noreply.github.com>
2020-02-13 02:15:29 +0100

    Update DefaultController.php

--- src/TalentBundle/Controller/DefaultController.php
+++ src/TalentBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('@Talent/Default/profile.html.twig');
+        return $this->render('@Talent/Default/index.html.twig');
     }
 }
