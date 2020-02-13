2e7914c492fc528e10f200511cc9698a7699f845
Anis Bouaziz <bouaziz609@gmail.com>
2020-02-13 17:21:34 +0100

    competition_list for user and admin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new CompetitionsBundle\CompetitionsBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
