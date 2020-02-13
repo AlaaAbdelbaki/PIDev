ae786b1f2f2f7fb9240d88c837aaf8897654e17d
hela ghedass <44788371+helaghedass@users.noreply.github.com>
2020-02-13 16:18:55 +0100

    crud ticket

--- src/EventsBundle/Controller/DefaultController.php
+++ src/EventsBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace EventsBundle\Controller;
 
+use AppBundle\Entity\event;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
@@ -9,5 +10,12 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('EventsBundle:Default:index.html.twig');
+    }
+
+    public function afficheAction()
+    {
+        $tab=$this->getDoctrine()->getRepository(event::class)->findAll();
+
+        return $this->render('@Events/default/affiche.html.twig',array('t'=>$tab));
     }
 }
