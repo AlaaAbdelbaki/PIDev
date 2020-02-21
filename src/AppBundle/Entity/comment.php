45f0395bcf3f02b29372a1868be780e8f7e5a300
AlaaAbdelbaki <44524232+AlaaAbdelbaki@users.noreply.github.com>
2020-02-21 02:27:58 +0100

    Implemented FOSComments

--- src/AppBundle/Entity/comment.php
+++ src/AppBundle/Entity/comment.php
@@ -4,6 +4,8 @@ namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\CommentBundle\Entity\Comment as BaseComment;
+use FOS\CommentBundle\Model\SignedCommentInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 
 
@@ -15,13 +17,11 @@ use FOS\CommentBundle\Entity\Comment as BaseComment;
  * @ORM\Table(name="comment")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\commentRepository")
  */
-class comment extends BaseComment
+class comment extends BaseComment implements SignedCommentInterface
 {
     /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer")
      * @ORM\Id
+     * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
@@ -33,149 +33,30 @@ class comment extends BaseComment
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Thread")
      */
     protected $thread;
-
     /**
-     * @var \DateTime
+     * Author of the comment
      *
-     * @ORM\Column(name="postdate", type="date")
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @var User
      */
-    private $postdate;
+    protected $author;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="author", type="string", length=255)
-     */
-    private $author;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="content", type="text")
-     */
-    private $content;
-
-    /**
-     * @return mixed
-     */
-    public function getUser()
-    {
-        return $this->user;
-    }
-
-    /**
-     * @param mixed $user
-     */
-    public function setUser($user)
-    {
-        $this->user = $user;
-    }
-
-    /**
-     * @ORM\ManyToOne(targetEntity="article", inversedBy="comments")
-     * @ORM\JoinColumn(name="article_id", referencedColumnName="id")
-     */
-    private $article;
-    /**
-     * @ORM\ManyToOne(targetEntity="User", inversedBy="comments")
-     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
-     */
-    private $user;
-
-    /**
-     * @return mixed
-     */
-    public function getArticle()
-    {
-        return $this->article;
-    }
-
-    /**
-     * @param mixed $article
-     */
-    public function setArticle($article)
-    {
-        $this->article = $article;
-    }
-    /**
-     * Get id
-     *
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set postdate
-     *
-     * @param \DateTime $postdate
-     *
-     * @return comment
-     */
-    public function setPostdate($postdate)
-    {
-        $this->postdate = $postdate;
-
-        return $this;
-    }
-
-    /**
-     * Get postdate
-     *
-     * @return \DateTime
-     */
-    public function getPostdate()
-    {
-        return $this->postdate;
-    }
-
-    /**
-     * Set author
-     *
-     * @param string $author
-     *
-     * @return comment
-     */
-    public function setAuthor($author)
+    public function setAuthor(UserInterface $author)
     {
         $this->author = $author;
-
-        return $this;
     }
 
-    /**
-     * Get author
-     *
-     * @return string
-     */
     public function getAuthor()
     {
         return $this->author;
     }
 
-    /**
-     * Set content
-     *
-     * @param string $content
-     *
-     * @return comment
-     */
-    public function setContent($content)
+    public function getAuthorName()
     {
-        $this->content = $content;
+        if (null === $this->getAuthor()) {
+            return 'Anonymous';
+        }
 
-        return $this;
-    }
-
-    /**
-     * Get content
-     *
-     * @return string
-     */
-    public function getContent()
-    {
-        return $this->content;
+        return $this->getAuthor()->getUsername();
     }
 }
