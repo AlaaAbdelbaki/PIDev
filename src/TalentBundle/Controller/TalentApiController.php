52a5414a12766169c03504bf9bafcec26a242eda
AlaaAbdelbaki <44524232+AlaaAbdelbaki@users.noreply.github.com>
2020-05-05 22:23:15 +0100

    added Api Controllers

---
+++ src/TalentBundle/Controller/TalentApiController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TalentBundle\Controller;
+
+use AppBundle\Entity\video;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class TalentApiController extends Controller
+{
+
+
+
+}
